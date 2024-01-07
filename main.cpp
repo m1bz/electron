4acab67d4306417803af08cf7fc639d06089023b
m1bz <mihai.bd@yahoo.com>
2024-01-07 12:43:04 +0200

    revamp menu deisgn + interactive buttons

--- main.cpp
+++ main.cpp
@@ -1,39 +1,60 @@
+#include <graphics.h>
 #include <iostream>
 #include <fstream>
 #include <filesystem>
-#include <graphics.h>
 #include <cmath>
-
-#define MAX1 10 // nr maxim de puncte de legatura pe care le poate avea o piesa
-#define MAX2 50 // nr maxim de comenzi de desenare pentru realizarea unei piese si nr maxim de piese pe care il putem incarca
-#define PI 3.14159 //self explanatory
+#define MAX1 10
+#define MAX2 25
+#define MAX_PLACED_PIECES 25
+#define PI 3.14159
+#define MCOLOR1 COLOR(15,255,80) //neon green
+#define MCOLOR2 COLOR(4, 55, 242) // blue
+#define MCOLOR3 COLOR (31, 81, 255) // neon blue
 
 using namespace std;
 
-
-struct xynodpiese // structura unde se salveaza coordonatele punctului de legatura al unei piese
+struct xynodpiese
 {
-    float x,y;
+    float x, y;
 };
-struct desen // salveaza tipul de desen si coordonatele
+
+struct Desen
 {
     char tipfigura;
-    float x1,y1,x2,y2;
+    float x1, y1, x2, y2;
 };
 
-struct piesa // contine informatiile unei piese
+struct piesa
 {
-    char name[100]; // numele piesei
-    int nrnod; // cate puncte de legatura are nodul respectiv
-    xynodpiese nodpiesa[MAX1]; // accesam pozitia punctului de legatura x,y
-    int nrcomenzidesen; // nr comenzilor de desen pentru a realiza piesa respectiva
-    desen comanda[MAX2];
+    char name[100];
+    int nrnod;
+    xynodpiese nodpiesa[MAX1];
+    int nrcomenzidesen;
+    Desen comanda[MAX2];
 };
 
-piesa piese[MAX2]; // vectorul in care salvam piesele vectorul este piese nu piesa()
+struct PlacedPiece
+{
+    int index;
+    float x, y;
+    int rotation;
+};
+
+piesa piese[MAX2];
+PlacedPiece placedPieces[MAX_PLACED_PIECES];
 int nrPiese = 0;
-int width = 1600, height = 900;
-int zoom = 10.0;
+int nrPlacedPieces = 0;
+int width = 1600, height = 900; // 16:9 resolutions: 1920 x 1080, 1600 x 900, 1280 x 720
+float zoom = 20.0;
+int c=8; //number of buttons
+int b=15; //by what we divide height and other variables
+const char *bnames[] = { "INTRODUCE", /*TEXT #1 BUTTON*/ "STERGE", /*TEXT #2 BUTTON*/
+                         "INSTRUMENTE", /*TEXT #3 BUTTON*/ "", /*TEXT #4 BUTTON*/
+                         "", /*TEXT #5 BUTTON*/ "", /*TEXT #6 BUTTON*/
+                         "INAPOI", /*TEXT #7 BUTTON*/ "IESI", /*TEXT #8 BUTTON*/
+                       };
+const char *bcarac[2]= {"UNGHI:", "MARIME:"};
+
 void citirePiesa(const string& filePath, piesa& p)
 {
     ifstream file(filePath);
@@ -48,7 +69,6 @@ void citirePiesa(const string& filePath, piesa& p)
     for (int i = 0; i < p.nrcomenzidesen; ++i)
     {
         file >> p.comanda[i].tipfigura;
-
         file >> p.comanda[i].x1 >> p.comanda[i].y1 >> p.comanda[i].x2 >> p.comanda[i].y2;
     }
 }
@@ -62,8 +82,20 @@ void incarcapiesele(const string& directoryPath)
     }
 }
 
+void menustyle()
+{
+    setlinestyle(0,0,2);
+    setcolor(MCOLOR3);
+}
 
-void roteste(float &x, float &y, float theta) {
+void resetstyle() //used to change from the menu style to normal style
+{
+    setlinestyle(0,0,0);
+    setcolor(WHITE);
+}
+
+void roteste(float &x, float &y, float theta)
+{
     float dx = x;
     float dy = y;
     x = dx*cos(theta) - dy*sin(theta);
@@ -94,24 +126,18 @@ void DesenDreptunghi(piesa P, int i, float x, float y, float unghi)
     rectangle(x_1 * zoom + x, y_1 * zoom + y, x_2 * zoom + x, y_2 * zoom + y);
 }
 
-void DesenElipsa(piesa P,int i, float x, float y,float unghi)
+void DrawCircle(piesa P,int i, float x, float y)
 {
-    float x_1 = P.comanda[i].x1;
-    float y_1 = P.comanda[i].y1;
-    float x_2 = P.comanda[i].x2;
-    float y_2 = P.comanda[i].y2;
-    roteste(x_1, y_1, unghi);
-    ellipse(x_1 * zoom + x, y_1 * zoom + y, 0, 360, x_2 * zoom, y_2 * zoom);
+    circle(x+P.comanda[i].x2*zoom,y+P.comanda[i].y2*zoom,P.comanda[i].x1*zoom);
 }
 
-void DesenArc(piesa P,int i, float x, float y,float unghi)
+void DrawButton(int xtop, int ytop, int xbottom, int ybottom)
 {
-    float x_1 = P.comanda[i].x1;
-    float y_1 = P.comanda[i].y1;
-    float x_2 = P.comanda[i].x2;
-    float y_2 = P.comanda[i].y2;
-    roteste(x_1, y_1, unghi);
-    arc(x_1 * zoom + x, y_1 * zoom + y, -90, 90, x_2 * zoom);
+    menustyle();
+    rectangle(xtop, ytop, xbottom, ybottom);
+    setlinestyle(0,0,1);
+    rectangle(xtop+2, ytop+2, xbottom-3, ybottom-3);
+    resetstyle();
 }
 
 void desen(piesa P, float x, float y, float unghi)
@@ -126,46 +152,107 @@ void desen(piesa P, float x, float y, float unghi)
             DesenDreptunghi(P, i, x, y, unghi);
             break;
         case 'O':
-            DesenElipsa(P, i, x, y, unghi);
-            break;
-        case 'A':
-            DesenArc(P, i, x, y, unghi);
+            DrawCircle(P,i,x,y);
             break;
         }
 }
 
-
-void prelucrarepartesus()
+void printtext(int x, int y, const char* text)
 {
-    int ylinie = height * 10 / 100;
-    line(0, ylinie, width, ylinie);
-    for(int i = 0; i <= nrPiese; i++)
-    {
-        line((float)width / nrPiese * i, 0,(float)width / nrPiese * i, ylinie);
-        for(int j = 0; j < piese[i].nrcomenzidesen; j++)
-        {
-            desen(piese[i], (float)width / nrPiese *(i+0.5), ylinie / 2, 50);// aici modifici 0 daca vrei sa schimbi unghiunl pieselor din meniu, nu recomand totusi
-        }
-    }
-}
-
-
-
-void initializare()
-{
-    prelucrarepartesus();
+    settextstyle(3,HORIZ_DIR,3);
+    setcolor(MCOLOR3);
+    char temp[100];
+    strcpy(temp, text);
+    outtextxy(x - textwidth(temp) / 2, y - textheight(temp) / 2, temp);
     setcolor(WHITE);
 }
 
+void prelucrarepartesus()
+{
+    menustyle();
+    int yline = height/b;
+    line(0, yline, width, yline);
+    line(0, yline*(b-1), width, yline*(b-1));
+    resetstyle();
+    for(int i = 0; i < 3; i++)
+    {
+        DrawButton(width/c*i, 1.5, width/c*(i+1), yline);// /c - how many buttons can fit; 1.5 - for fitting
+        printtext((width/c*i+width/c*(i+1))/2,yline/2,bnames[i]);
+    }
+    for(int i = c-1; i > c-3; i--)
+    {
+        DrawButton(width/c*i, 1.5, width/c*(i+1), yline);// /c - how many buttons can fit; 1.5 - for fitting
+        printtext((width/c*i+width/c*(i+1))/2,yline/2,bnames[i]);
+    }
+}
 
+void Backtomenu()
+{
+
+}
+
+void CheckIfPieceIsSelected()
+{
+
+}
+
+void DeletePiece()
+{
+    CheckIfPieceIsSelected();
+}
+
+
+void Lclick_handler(int x, int y)
+{
+    if(x < width/c*c && width/c*(c-1) < x && 0 < y && y < height/b)
+    {
+        closegraph();
+        getch();
+    }
+    if(x < width/c*(c-1) && width/c*(c-2) < x && 0 < y && y < height/b)
+        Backtomenu();
+    if(x < width/c*2 && width/c < x && 0 < y && y < height/b)
+        DeletePiece();
+}
+
+void Move_handler(int x, int y)
+{
+    int ok=1;
+    if(x < width/c && 0 < x && 0 < y && y < height/b)
+        for(int i = 0; i < nrPiese; i++)
+        {
+            DrawButton(0, height/b*(i+1), width/c, height/b*(i+2));
+            desen(piese[i], width/c/2, (height/b*(i+1)+height/b*(i+2))/2, 0);
+            ok=0;
+        }
+        else
+         {
+            if(ok==0 && x < width/c && 0 < x && 0 < y && y < height/b*(nrPiese+1))
+                outtextxy(400,400,"keep");
+            else
+            {
+                cleardevice();
+                prelucrarepartesus();
+            }
+        }
+    if(x < width/c && 0 < x && 0 < y && y < height/b)
+    {
+
+    }
+}
+
+void initializare()
+{
+    initwindow(width, height);
+    registermousehandler(WM_LBUTTONDOWN,Lclick_handler);
+    registermousehandler(WM_MOUSEMOVE,Move_handler);
+    delay(4000000);// so it doesnt instantly close the program
+}
 
 int main()
 {
-    string path = R"(C:\Users\miha\Documents\CodeBlocks\electron\piese electrice)"; // replace with your directory path
+    string path = R"(C:\Users\miha\Documents\CodeBlocks\electron\piese electrice)";
     incarcapiesele(path);
-    initwindow(width,height);
     initializare();
-    getch();
-    closegraph();
     return 0;
 }
